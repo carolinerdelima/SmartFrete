d13d5b83f1dc46008026da8475bd99303249ed07
carolinerdelima <carolinelima013@hotmail.com>
2025-08-05 11:08:23 -0300

    inserindo padrão de retorno de apis

--- smartfrete/routes/api.php
+++ smartfrete/routes/api.php
@@ -4,5 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\MetricsController;
 
+use Illuminate\Support\Carbon;
+
 Route::post('/quote', [QuoteController::class, 'store']);
 Route::get('/metrics',[MetricsController::class, 'index']);
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'SmartFrete API',
+        'datetime' => Carbon::now('America/Sao_Paulo')->toIso8601String(),
+    ]);
+});
